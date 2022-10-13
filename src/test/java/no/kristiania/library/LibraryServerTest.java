ba5330f2455ea1b957ab324ea354b6e2cc7e6b28
BilalStudy <biah001@student.kristiania.no>
2022-10-13 13:24:22 +0200

    🔴 return book list test implemented, showfrontpage test now works

--- src/test/java/no/kristiania/library/LibraryServerTest.java
+++ src/test/java/no/kristiania/library/LibraryServerTest.java
@@ -1,5 +1,6 @@
 package no.kristiania.library;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -9,18 +10,39 @@ import java.nio.charset.StandardCharsets;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LibraryServerTest {
 
+
+    private LibraryServer server;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        server = new LibraryServer(0);
+        server.start();
+    }
+
     @Test
     void shouldShowFrontPage() throws Exception {
-        LibraryServer server = new LibraryServer(0);
-        server.start();
-
         URL url = server.getURL();
 
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        assertThat(connection.getResponseCode().as(connection.getResponseMessage().isEqualTo(200)));
-        assertThat(connection.getInputStream().asString(StandardCharsets.UTF_8).contains("<h1>Kristiania Library</h1>"));
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(server.getURL(),"/").openConnection();
+
+        assertThat(connection.getResponseCode()).as(connection.getResponseMessage())
+                .isEqualTo(200);
+        assertThat(connection.getInputStream()).asString(StandardCharsets.UTF_8)
+                .contains("<h1>Kristiania Library</h1>");
+    }
+
+
+    @Test
+    void shouldReturnBookList() throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(server.getURL(), "/api/books").openConnection();
+        assertThat(connection.getResponseCode()).as(connection.getResponseMessage())
+                .isEqualTo(200);
+        assertThat(connection.getInputStream()).asString(StandardCharsets.UTF_8)
+                .contains("{\"title\": \"Example Book\"");
     }
 }
