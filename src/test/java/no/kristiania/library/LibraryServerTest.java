df2b0fb76ac1ba37886c9bc987acd862bddacad7
BilalStudy <biah001@student.kristiania.no>
2022-10-13 02:55:46 +0200

    🔴 updating pom and making test class have assertj and junit

--- src/test/java/no/kristiania/library/LibraryServerTest.java
+++ src/test/java/no/kristiania/library/LibraryServerTest.java
@@ -1,4 +1,22 @@
 package no.kristiania.library;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LibraryServerTest {
+
+    @Override
+    void shouldShowFrontPage() throws IOException {
+        LibraryServer server = new LibraryServer(0);
+
+        URL url = server.getURL();
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        assertThat(connection.getResponseCode().as(connection.getResponseMessage().isEqualTo(200)));
+        assertThat(connection.getInputStream().asString().contains("<h1>Kristiania Library</h1>"));
+    }
 }
